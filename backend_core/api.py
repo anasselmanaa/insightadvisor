b327b414c4ca50b772b3a4257c7c9dcd53c03e97
anasselmanaa <anasselmanaa7@gmail.com>
2026-04-10 18:55:33 +0800

    Fix CORS - remove duplicate middleware

--- backend_core/api.py
+++ backend_core/api.py
@@ -16,18 +16,9 @@ from backend_core.database import init_db
 
 app = FastAPI(title="RetailIQ Backend", version="2.0")
 
-from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
